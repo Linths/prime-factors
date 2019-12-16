d959bed08beb87fd1a9b831c52a5ae761fa463cc
EricvanSchaik <schaikejgv@gmail.com>
2019-12-16 15:26:35 +0100

    refactoring!

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from src.generate_data import GenerateData
+from src.generate_data import generate_data
 from src.rns import RNS
 
 bit_length = 256
-data = GenerateData().generate_data(1000, bit_length)
+data = generate_data(1000, bit_length)
 rns = RNS(bit_length)
 data = rns.transform()
