542eae86540ebd02871e71dd19d5a7b995b33f94
Lindsay Kempen <l.a.o.kempen@student.utwente.nl>
2020-01-06 17:34:06 +0100

    Add detailed logging (but check if it's correct)

--- src/main.py
+++ src/main.py
@@ -45,14 +45,46 @@ def test(models):
     first_inp = inputs[0]
     print(first_inp)
     
+    bools = []
+    totalLikLog = 0
+    uniformLikLog = 0
     # Per modulo, predict the correct residue
     for j, model in models.items():
         print(f"using model {j}")
         possible_outputs = {res : [math.cos(2*math.pi*res/j), math.sin(2*math.pi*res/j)] for res in range(1,j)}
         likelihoods = {res : model.likelihood(DataPair(first_inp.input, out, first_inp.input_OG, res)) for res, out in possible_outputs.items()}
-        print(likelihoods)
-        print(f"most likely res = {max3(likelihoods)}\nactual res = {first_inp.output_OG % j}\nactual res with sin/cos = {first_inp.output}")
+        norm_likelihoods = {res : lik / sum(likelihoods) for res, lik in likelihoods.items()}
+        print(norm_likelihoods)
+        actual = first_inp.output_OG % j
+        predicted = max(norm_likelihoods, key=norm_likelihoods.get)
+        print(f"most likely res = {max3(norm_likelihoods)}\nactual res = {actual}")
+        actualIsAboveAvg = norm_likelihoods.get(actual) > 1/(j-1)
+        print(actualIsAboveAvg)
+        bools.append(actualIsAboveAvg)
+        # predictedIsCloseToActual = findPeriodicDist(actual, predicted, j) <= math.ceil((j-1) / 3)
+        # print(predictedIsCloseToActual)
+        # bools.append(predictedIsCloseToActual)
+        print(norm_likelihoods.get(actual))
+        totalLikLog += np.log(norm_likelihoods.get(actual))
+        uniformLikLog += np.log(1/(j-1))
         print()
+    
+    print(f"{bools}\n#True = {bools.count(True)}\n#False = {bools.count(False)}")
+    print(f"totalLikLog = {totalLikLog}\nuniformLikLog = {uniformLikLog}")
+
+
+def findPeriodicDist(n1, n2, j):
+    '''Find the distance between n1 and n2, where numbers are spread from 1..(j-1) and where 1 and j-1 are neighbours. (It wraps and we skip 0!)
+    Assumes n1 != j, n1 != 0, n2 != j, n2 != 0 '''
+    # Ensure n2 >= n1
+    if n2 < n1:
+        n1, n2 = n2, n1
+    # Find the distance without using 'wrapping'
+    dist1 = n2 - n1
+    # Find the distance when 'wrapping' (passing j-1 and 1)
+    dist2 = (j-1) - n2 + n1 
+    return min(dist1, dist2)
+
 
 def max3(dictio):
     ''' Determine the top 3 residues with the highest likelihood.
@@ -67,4 +99,4 @@ def max3(dictio):
 
 if __name__ == "__main__":
     models = train()
-    # test(models)
+    test(models)
