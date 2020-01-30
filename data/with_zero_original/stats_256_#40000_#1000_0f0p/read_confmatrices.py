47d5e67e69cafb3b6b6163b7bbf66c2e80e9cba5
Michiexb <mdj.peters@gmail.com>
2020-01-30 14:15:48 +0100

    Update read_confmatrices.py
    File to creates heatmaps updated to correspond to latest summary.txt update

--- data/with_zero_original/stats_256_#40000_#1000_0f0p/read_confmatrices.py
+++ data/with_zero_original/stats_256_#40000_#1000_0f0p/read_confmatrices.py
@@ -17,11 +17,12 @@ for i,mod in enumerate(mods):
     repl = np.arange(0,mod-1)
     
     df_mod = pd.read_excel('ConfMatrices.xlsx', sheet_name='mod%d'%mod, header=None)
-    if mod<13:
-        df_mod.columns = res
-        df_mod = df_mod.rename(index=dict(zip(repl,res)))
-        y_min = mod-1
-    else: y_min = mod
+    # if mod<13:
+        # df_mod.columns = res
+        # df_mod = df_mod.rename(index=dict(zip(repl,res)))
+        # y_min = mod-1
+    # else: 
+    y_min = mod
     
     s = int(mod/2)
     plt.figure(i, figsize=(s,s))
